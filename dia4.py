fa11be110e2a0466171dd07b0fd612bccf005772
Erick Milan <erickmilan1002@gmail.com>
2022-04-14 19:49:24 -0500

    objetos

--- dia4.py
+++ dia4.py
@@ -1,1 +1,23 @@
-#
+#crear clase
+class Automovil:
+    #!crear atributos
+    def __init__(self,aa,pl,col,mar):
+        self.año = aa
+        self.placa = pl
+        self.col = col
+        self.marca = mar
+    #metodos
+    def encender(self):
+        print("encender: " + self.marca)
+    def avanzar(self):
+        print("avanzar: " + self.marca)
+    def acelerar(self):
+        print("acelerar: " + self.marca)
+    def frenar(self):
+        print("frenar: " + self.marca)
+#crer un objeto
+vw = Automovil(1970,"VH-159","amarillo","volkswagen")
+tico = Automovil(1970,"VH-159","rojo","tico")
+#utilizar el objeto
+vw.encender()
+tico.frenar()
