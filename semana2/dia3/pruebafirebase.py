6d33a704f7d7151310245936555d3660ebeca90f
Erick Milan <erickmilan1002@gmail.com>
2022-04-22 16:13:24 -0500

    pruebas

--- semana2/dia3/pruebafirebase.py
+++ semana2/dia3/pruebafirebase.py
@@ -1,5 +1,16 @@
+# Para conectarse a firebase
 import firebase_admin
 from firebase_admin import credentials
 
 cred = credentials.Certificate("token.json")
 firebase_admin.initialize_app(cred)
+### para conectarse a la firestore
+from firebase_admin import firestore
+
+db = firestore.client()
+
+colProyectos = db.collection('proyecto')
+docProyectos = colProyectos.get()
+
+for doc in docProyectos:
+    print(doc.to_dict())
