0cc422a8c8b98db16f773ecae08144f15346ab89
Erick Milan <erickmilan1002@gmail.com>
2022-05-12 17:01:03 -0500

    dia sabado

--- semana4/dia4/web/urls.py
+++ semana4/dia4/web/urls.py
@@ -6,6 +6,15 @@ app_name = 'web'
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('producto',views.producto,name='producto')
+    path('producto/<int:producto_id>',views.producto,name='producto'),
+    path('productosPorCategoria/<int:categoria_id>',views.productosPorCategoria,name='productosPorCategoria'),
+    path('carrito',views.carrito,name='carrito'),
+    path('agregarCarrito/<int:producto_id>',views.agregarCarrito,name='agregarCarrito'),
+    path('eliminarProductoCarrito/<int:producto_id>',views.eliminarProductoCarrito,name='eliminarProductoCarrito'),
+    path('limpiarCarrito',views.limpiarCarrito,name='limpiarCarrito'),
+    path('login',views.loginUsuario,name='loginUsuario'),
+    path('crearUsuario',views.crearUsuario,name='crearUsuario'),
+    path('cuenta',views.cuentaUsuario,name='cuentaUsuario'),
+    path('actualizarCliente',views.actualizarCliente,name='actualizarCliente'),
+    path('registrarPedido',views.registrarPedido,name='registrarPedido')
 ]
-
