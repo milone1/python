1902ce2fc4ea727937b54519cac57652c0704a2e
Erick Milan <erickmilan1002@gmail.com>
2022-05-16 23:13:28 -0500

    comanda

--- semana5/dia3/pos_backend/api/views.py
+++ semana5/dia3/pos_backend/api/views.py
@@ -1,15 +1,24 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from rest_framework.permissions import IsAuthenticated
+
 from .models import (
-    Categoria,Mesa,Plato
+    Categoria,Mesa,Plato,Pedido
 )
-from .serializers import (
-    CategoriaSerializers,
-    MesaSerializers,
-    PlatoSerializers
+
+from .serializers import(
+    CategoriaPlatosSerializer,
+    CategoriaSerializer,
+    MesaSerializer,
+    PedidoSerializerGET,
+    PedidoSerializerPOST,
+    PlatoSerializer
 )
 
 class IndexView(APIView):
+
+    permission_classes = [IsAuthenticated]
 
     def get(self,request):
         context = {
@@ -22,10 +31,11 @@ class CategoriaView(APIView):
 
     def get(self,request):
         dataCategoria = Categoria.objects.all()
-        serCategoria = CategoriaSerializers(dataCategoria,many=True)
+        serCategoria = CategoriaSerializer(dataCategoria,many=True)
+
         context = {
-            'ok' : True,
-            'content' : serCategoria.data
+            'ok':True,
+            'content':serCategoria.data
         }
 
         return Response(context)
@@ -34,21 +44,62 @@ class MesaView(APIView):
 
     def get(self,request):
         dataMesa = Mesa.objects.all()
-        serMesa = MesaSerializers(dataMesa,many=True)
+        serMesa = MesaSerializer(dataMesa,many=True)
+
         context = {
-            'ok' : True,
-            'content' : serMesa.data
+            'ok':True,
+            'content':serMesa.data
         }
 
         return Response(context)
 
 class PlatoView(APIView):
+
     def get(self,request):
         dataPlato = Plato.objects.all()
-        serPlato = PlatoSerializers(dataPlato,many=True)
+        serPlato = PlatoSerializer(dataPlato,many=True)
+
         context = {
-            'ok' : True,
-            'content' : serPlato.data
+            'ok':True,
+            'content':serPlato.data
         }
 
         return Response(context)
+
+class CategoriaPlatosView(APIView):
+
+    def get(self,request,categoria_id):
+        dataCategoria = Categoria.objects.get(pk=categoria_id)
+        serCategoriaPlatos = CategoriaPlatosSerializer(dataCategoria)
+
+        context = {
+            'ok':True,
+            'content':serCategoriaPlatos.data
+        }
+
+        return Response(context)
+
+class PedidoView(APIView):
+
+    def get(self,request):
+        dataPedido = Pedido.objects.all()
+        serPedido = PedidoSerializerGET(dataPedido,many=True)
+
+        context = {
+            'ok':True,
+            'pedidos':serPedido.data
+        }
+
+        return Response(context)
+
+    def post(self,request):
+        serPedido = PedidoSerializerPOST(data=request.data)
+        serPedido.is_valid(raise_exception=True)
+        serPedido.save()
+
+        context = {
+            'ok':True,
+            'content':serPedido.data
+        }
+        return Response(context)
+
